249ac629ed79bc57a25a353b9b69930f2221cf68
Zack Dong <zackdong@Zacks-MacBook-Air.local>
2022-09-23 09:31:55 -0500

    Added changes to tests

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -24,6 +24,7 @@ class BooksDataSourceTester(unittest.TestCase):
     # test sorted by surname
     # test sorted by surname breaking ties
     # test non-string input
+    
 
     def test_author_case_insensitivity(self):
         authors = self.data_source.authors('Pratchett')
@@ -46,7 +47,24 @@ class BooksDataSourceTester(unittest.TestCase):
     def test_bad_input(self):
         
 
+    def test_unique_book(self):
+        books = self.data_source.books('Blackout')
+        self.asserTrue(len(books) == 1)
+        self.assertTrue(authors[0] == Book('Blackout')
 
+    def test_book_case_insensitivity(self):
+        books_lowercase = self.data_source.books('emma')
+        books_uppercase = self.data_source.books('EMMA')
+        self.assertEqual(books_lowercase, books_uppercase)
+
+    def test_no_search_books(self):
+        self.data_source = BooksDataSource(
+ 
+    #books
+    # test case insensitivity
+    # test serach text = NOne
+    #test sorted by year
+    # test sorted by title
 if __name__ == '__main__':
     unittest.main()
 
