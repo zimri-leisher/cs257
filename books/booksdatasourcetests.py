5b9143b922e6ac6623c1e22f5c8915178e3f4cab
zimri-leisher <zimri.leisher@gmail.com>
2022-09-23 10:04:43 -0500

    Add some more tests

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -26,17 +26,17 @@ class BooksDataSourceTester(unittest.TestCase):
     # test non-string input
     
 
-    def test_author_case_insensitivity(self):
+    def test_authors_case_insensitivity(self):
         authors = self.data_source.authors('Pratchett')
         authors_lowercase = self.data_source.authors('pratchett')
         self.assertEqual(authors, authors_lowercase)
     
-    def test_no_search_text(self):
+    def test_authors_no_search_text(self):
         small_data_source = BooksDataSource('smalldatasource.csv')
         all_authors = small_data_source.authors(None)
         #... assert the authors match
 
-    def test_sorted_surname(self):  
+    def test_authors_sorted_surname(self):  
         small_data_source = BooksDataSource('smalldatasource.csv')
         authors = small_data_source.authors(None)
         #... assert that the authors are sorted by surname
@@ -44,21 +44,30 @@ class BooksDataSourceTester(unittest.TestCase):
         #... assert that the authors are sorted by surname
         # and test breaking ties by given name
 
-    def test_bad_input(self):
-        pass 
+    def test_authors_bad_input(self):
+        with self.assertRaises(TypeError):
+            self.data_source.authors(True)
 
     def test_unique_book(self):
         books = self.data_source.books('Blackout')
         self.asserTrue(len(books) == 1)
         self.assertTrue(authors[0] == Book('Blackout')
 
-    def test_book_case_insensitivity(self):
+    def test_books_case_insensitivity(self):
         books_lowercase = self.data_source.books('emma')
         books_uppercase = self.data_source.books('EMMA')
         self.assertEqual(books_lowercase, books_uppercase)
 
-    def test_no_search_books(self):
-        self.data_source = BooksDataSource(
+    def test_books_no_search_text(self):
+        small_data_source = BooksDataSource('smalldatasource.csv')
+        books = small_data_source.books(None)
+        # ... test that the books are right size, right order, etc
+
+    def test_books_sorted_by_year(self):
+        pass
+
+    def test_books_sorted_by_title(self):
+        pass
  
     #books
     # test case insensitivity
