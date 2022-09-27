32ab760821ba776ac5f8d4a5f93f1b6b787d47b2
Zack Dong <zackdong@Zacks-MacBook-Air.local>
2022-09-27 17:26:46 -0500

    Changes to booksdatasource class

--- books/booksdatasource.py
+++ books/booksdatasource.py
@@ -8,6 +8,8 @@
 '''
 
 import csv
+import re
+
 
 class Author:
     def __init__(self, surname='', given_name='', birth_year=None, death_year=None):
@@ -36,6 +38,35 @@ class Book:
 
 class BooksDataSource:
     def __init__(self, books_csv_file_name):
+        self.authors_list = []
+        self.books_list = []
+        with open(books_csv_file_name) as file:
+            csvreader = csv.reader(file)
+            for row in csvreader:
+                #Splits if there multiple authors
+                author_description = row[2].split(' and ')
+                for authors in author_description:
+                    #Splits into name, birthyear, and deathyear
+                    authors = re.split('[(-]', authors)
+                    author_name = authors[0].split()
+                    first_name = ""
+                    #Case for 2-part first names
+                    if len(author_name) > 2:
+                        surname = author_name[-1]
+                        for i in range(len(author_name) - 1):
+                            first_name = first_name + author_name[i] + " "
+                        #remove whitespaces on right side
+                        first_name = first_name.rstrip()
+                    else:
+                        first_name = author_name[0]
+                        surname = author_name[1]
+                    birth_year = authors[1]
+                    death_year = authors[2].replace(')', '')
+                    death_year = death_year if death_year != "" else None
+                    
+                    self.authors_list.append(Author(surname, first_name, birth_year, death_year))
+                    self.books_list.append(Book(row[0], row[1]))
+
         ''' The books CSV file format looks like this:
 
                 title,publication_year,author_description
@@ -51,13 +82,18 @@ class BooksDataSource:
         '''
         pass
 
+
     def authors(self, search_text=None):
+        authors_search = []
+        for author in self.authors_list:
+            if search_text in (author.given_name + " " + author.surname):
+                authors_search.append(author)
         ''' Returns a list of all the Author objects in this data source whose names contain
             (case-insensitively) the search text. If search_text is None, then this method
             returns all of the Author objects. In either case, the returned list is sorted
             by surname, breaking ties using given name (e.g. Ann Brontë comes before Charlotte Brontë).
         '''
-        return []
+        return authors_search
 
     def books(self, search_text=None, sort_by='title'):
         ''' Returns a list of all the Book objects in this data source whose
@@ -71,9 +107,13 @@ class BooksDataSource:
                 default -- same as 'title' (that is, if sort_by is anything other than 'year'
                             or 'title', just do the same thing you would do for 'title')
         '''
-        return []
+        filtered_books = []
+        for book in self.books_list:
+            
+        return filtered_books
 
     def books_between_years(self, start_year=None, end_year=None):
+        
         ''' Returns a list of all the Book objects in this data source whose publication
             years are between start_year and end_year, inclusive. The list is sorted
             by publication year, breaking ties by title (e.g. Neverwhere 1996 should
@@ -85,3 +125,15 @@ class BooksDataSource:
             should be included.
         '''
         return []
+
+def main():
+    BooksDataSource('/Users/zackdong/Documents/CS257/cs257-zimri/books/smalldatasource.csv')
+    # parse the args
+    # start with the full list of books
+    # for each filter (title, author, date)
+    #   remove books from the full list which don't match the filter
+    # sort by the specified sorting method (in the args)
+    # for each book remaining 
+
+if __name__ == "__main__":
+    main()
