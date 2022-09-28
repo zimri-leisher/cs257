f820be1b47a48dcbff7dc86732486a5728750076
zimri-leisher <zimri.leisher@gmail.com>
2022-09-28 12:01:38 -0500

    Add CLI for books.py

---
+++ books/books.py
@@ -0,0 +1,65 @@
+from booksdatasource import *
+
+
+def main():
+    arg_parse = ArgumentParser()
+    # arg_parse.add_argument("-h", "--help", help="Print usage message", action="store_true")
+    arg_parse.add_argument("-i", "--input", help="Loads books from the CSV file INPUT.",
+                           type=str, default="books.csv")
+    arg_parse.add_argument("-t", "--title", help="Filters results to books whose title matches TITLE.",
+                           type=str)
+    arg_parse.add_argument("-a", "--author", help="Filters results to books whose author's name matches AUTHOR.",
+                           type=str)
+    arg_parse.add_argument("-d", "--date",
+                           help="In the format START_YEAR[,END_YEAR]. Filters results to books whose start year is "
+                                "after START_YEAR inclusive, and before END_YEAR inclusive, if it is specified",
+                           type=str)
+    arg_parse.add_argument("-s", "--sort", help="SORT = title|date. "
+                                                "Sorts the results by either the title or the date published.",
+                           type=str, default="title")
+    arg_parse.add_argument("-o", "--output",
+                           help="Specifies the output format for the results. "
+                                "This string can consist of any combination of 0 or 1 of the following characters: "
+                                "'a', 't', 'd', which correspond to printing the author, title and date published "
+                                "respectively. For example, if --output dt is passed in, the output for each book "
+                                "matching the filters will be the date published followed by the title.",
+                           type=str, default="tad")
+    args = arg_parse.parse_args()
+    filters = []
+    if args.title:
+        filters.append(TitleFilter(args.title))
+    if args.author:
+        filters.append(AuthorFilter(args.author))
+    if args.date:
+        start_year, *end_year = [s.replace("(", "").replace(")", "").replace(" ", "") for s in args.date.split(",")]
+        start_year = int(start_year)
+        end_year = int(end_year[0]) if len(end_year) > 0 and end_year[0] != '' else None
+        filters.append(PublicationYearFilter(start_year, end_year))
+    for c in args.output:
+        if c not in ['a', 't', 'd']:
+            raise Exception("Invalid format character " + c + ", valid characters are 'a', 't', 'd'")
+    compound_filter = CompoundFilter(filters)
+    data = BooksDataSource(args.input)
+    matching = data.filter(compound_filter)
+    if args.sort == "title":
+        matching = sorted(matching, key=lambda b: b.publication_year)
+        matching = sorted(matching, key=lambda b: b.title)
+    elif args.sort == "date":
+        matching = sorted(matching, key=lambda b: b.title)
+        matching = sorted(matching, key=lambda b: b.publication_year)
+    else:
+        raise Exception("Invalid sort type " + args.sort)
+    for book in matching:
+        formatted_book = []
+        for format_char in args.output:
+            if format_char == 'a':
+                formatted_book.append(", ".join([str(author) for author in book.authors]))
+            elif format_char == 't':
+                formatted_book.append(str(book.title))
+            elif format_char == 'd':
+                formatted_book.append(str(book.publication_year))
+        print(", ".join(formatted_book))
+
+
+if __name__ == "__main__":
+    main()
--- books/booksdatasource.py
+++ books/booksdatasource.py
@@ -1,73 +1,143 @@
 #!/usr/bin/env python3
-'''
+"""
     booksdatasource.py
     Jeff Ondich, 21 September 2022
 
     For use in the "books" assignment at the beginning of Carleton's
     CS 257 Software Design class, Fall 2022.
-'''
+"""
 
 import csv
 import re
+from argparse import ArgumentParser
+from typing import List
 
 
 class Author:
-    def __init__(self, surname='', given_name='', birth_year=None, death_year=None):
+    def __init__(self, given_name="", surname="", birth_year=None, death_year=None):
         self.surname = surname
         self.given_name = given_name
         self.birth_year = birth_year
         self.death_year = death_year
 
     def __eq__(self, other):
-        ''' For simplicity, we're going to assume that no two authors have the same name. '''
+        """ For simplicity, we're going to assume that no two authors have the same name. """
         return self.surname == other.surname and self.given_name == other.given_name
 
+    def __str__(self):
+        return f"{self.given_name} {self.surname} ({self.birth_year}-{self.death_year if self.death_year else ''})"
+
+
 class Book:
-    def __init__(self, title='', publication_year=None, authors=[]):
-        ''' Note that the self.authors instance variable is a list of
-            references to Author objects. '''
+    def __init__(self, title="", publication_year=None, authors=None):
+        """ Note that the self.authors instance variable is a list of
+            references to Author objects. """
+        if authors is None:
+            authors = []
         self.title = title
         self.publication_year = publication_year
         self.authors = authors
 
     def __eq__(self, other):
-        ''' We're going to make the excessively simplifying assumption that
+        """ We're going to make the excessively simplifying assumption that
             no two books have the same title, so "same title" is the same
-            thing as "same book". '''
+            thing as "same book". """
         return self.title == other.title
 
-class BooksDataSource:
-    def __init__(self, books_csv_file_name):
-        self.authors_list = []
-        self.books_list = []
-        with open(books_csv_file_name) as file:
-            csvreader = csv.reader(file)
-            for row in csvreader:
-                #Splits if there multiple authors
-                author_description = row[2].split(' and ')
-                for authors in author_description:
-                    #Splits into name, birthyear, and deathyear
-                    authors = re.split('[(-]', authors)
-                    author_name = authors[0].split()
-                    first_name = ""
-                    #Case for 2-part first names
-                    if len(author_name) > 2:
-                        surname = author_name[-1]
-                        for i in range(len(author_name) - 1):
-                            first_name = first_name + author_name[i] + " "
-                        #remove whitespaces on right side
-                        first_name = first_name.rstrip()
-                    else:
-                        first_name = author_name[0]
-                        surname = author_name[1]
-                    birth_year = authors[1]
-                    death_year = authors[2].replace(')', '')
-                    death_year = death_year if death_year != "" else None
-                    
-                    self.authors_list.append(Author(surname, first_name, birth_year, death_year))
-                    self.books_list.append(Book(row[0], row[1]))
+    def __str__(self):
+        return f"Book({self.title}, {self.publication_year}, " + "".join([str(author) for author in self.authors]) + ")"
 
-        ''' The books CSV file format looks like this:
+
+def _get_books_from_csv(csv_file):
+    books_list = []
+    with open(csv_file) as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            # Splits if there are multiple authors
+            authors = []
+            for author_description in row[2].split(" and "):
+                # Splits into name, birthyear, and deathyear
+                author_description = re.split("[(-]", author_description)
+                author_name = author_description[0].split()
+                first_name = ""
+                # Case for 2-part first names
+                if len(author_name) > 2:
+                    surname = author_name[-1]
+                    for i in range(len(author_name) - 1):
+                        first_name = first_name + author_name[i] + " "
+                    # remove whitespaces on right side
+                    first_name = first_name.rstrip()
+                else:
+                    first_name = author_name[0]
+                    surname = author_name[1]
+                birth_year = author_description[1] if len(author_description) > 1 else None
+                death_year = author_description[2].replace(")", "") if len(author_description) > 2 else None
+                death_year = death_year if death_year != "" else None
+                authors.append(Author(first_name, surname, birth_year, death_year))
+            books_list.append(Book(row[0], int(row[1]), authors))
+    return books_list
+
+
+def _get_authors_from_books(books):
+    authors = []
+    for book in books:
+        for author in book.authors:
+            if author not in authors:
+                authors.append(author)
+    return authors
+
+
+class BookFilter:
+    def check(self, book) -> bool:
+        pass
+
+
+class TitleFilter(BookFilter):
+
+    def __init__(self, title):
+        self.title = title
+
+    def check(self, book) -> bool:
+        return not self.title or self.title.lower() in book.title.lower()
+
+
+class AuthorFilter(BookFilter):
+
+    def __init__(self, author_match_str):
+        self.author_match_str = author_match_str
+
+    def check(self, book) -> bool:
+        return not self.author_match_str or any([
+            self.author_match_str.lower() in (author.given_name + " " + author.surname).lower()
+            for author in book.authors])
+
+
+class PublicationYearFilter(BookFilter):
+
+    def __init__(self, start_year, end_year):
+        self.start_year = int(start_year) if start_year else None
+        self.end_year = int(end_year) if end_year else None
+
+    def check(self, book) -> bool:
+        if self.start_year and int(book.publication_year) < self.start_year:
+            return False
+        if self.end_year and int(book.publication_year) > self.end_year:
+            return False
+        return True
+
+
+class CompoundFilter(BookFilter):
+
+    def __init__(self, filters):
+        self.filters = filters
+
+    def check(self, book) -> bool:
+        return all([f.check(book) for f in self.filters])
+
+
+class BooksDataSource:
+    def __init__(self, books_data):
+        """ The books CSV file format looks like this:
 
                 title,publication_year,author_description
 
@@ -79,24 +149,33 @@ class BooksDataSource:
             This __init__ method parses the specified CSV file and creates
             suitable instance variables for the BooksDataSource object containing
             a collection of Author objects and a collection of Book objects.
-        '''
-        pass
-
+        """
+        self.all_books = []
+        if isinstance(books_data, str):
+            # books_data is a csv file path
+            self.all_books = _get_books_from_csv(books_data)
+        elif isinstance(books_data, list):
+            self.all_books = books_data
+        else:
+            raise Exception("Invalid argument passed to BooksDataSource: " + books_data)
+        self.all_authors = _get_authors_from_books(self.all_books)
 
     def authors(self, search_text=None):
-        authors_search = []
-        for author in self.authors_list:
-            if search_text in (author.given_name + " " + author.surname):
-                authors_search.append(author)
-        ''' Returns a list of all the Author objects in this data source whose names contain
+        """ Returns a list of all the Author objects in this data source whose names contain
             (case-insensitively) the search text. If search_text is None, then this method
             returns all of the Author objects. In either case, the returned list is sorted
             by surname, breaking ties using given name (e.g. Ann Brontë comes before Charlotte Brontë).
-        '''
-        return authors_search
+        """
+        filtered = []
+        for author in self.all_authors:
+            if not search_text or search_text.lower() in (author.given_name + " " + author.surname).lower():
+                filtered.append(author)
+        filtered = sorted(filtered, key=lambda a: a.given_name)
+        filtered = sorted(filtered, key=lambda a: a.surname)
+        return filtered
 
-    def books(self, search_text=None, sort_by='title'):
-        ''' Returns a list of all the Book objects in this data source whose
+    def books(self, search_text=None, sort_by="title"):
+        """ Returns a list of all the Book objects in this data source whose
             titles contain (case-insensitively) search_text. If search_text is None,
             then this method returns all of the books objects.
 
@@ -106,15 +185,20 @@ class BooksDataSource:
                 'title' -- sorts by (case-insensitive) title, breaking ties with publication_year
                 default -- same as 'title' (that is, if sort_by is anything other than 'year'
                             or 'title', just do the same thing you would do for 'title')
-        '''
-        filtered_books = []
-        for book in self.books_list:
-            
-        return filtered_books
+        """
+        filtered = self.filter(TitleFilter(search_text))
+        if sort_by == "title":
+            # first sort by publication year, and then title so that the publication years break ties
+            filtered = sorted(filtered, key=lambda b: int(b.publication_year))
+            filtered = sorted(filtered, key=lambda b: b.title.lower())
+        else:
+            # first sort by title, and then publication year so that the title breaks ties
+            filtered = sorted(filtered, key=lambda b: b.title.lower())
+            filtered = sorted(filtered, key=lambda b: int(b.publication_year))
+        return filtered
 
     def books_between_years(self, start_year=None, end_year=None):
-        
-        ''' Returns a list of all the Book objects in this data source whose publication
+        """ Returns a list of all the Book objects in this data source whose publication
             years are between start_year and end_year, inclusive. The list is sorted
             by publication year, breaking ties by title (e.g. Neverwhere 1996 should
             come before Thief of Time 1996).
@@ -123,17 +207,13 @@ class BooksDataSource:
             should be included. If end_year is None, then any book published after or
             during start_year should be included. If both are None, then all books
             should be included.
-        '''
-        return []
+        """
+        filtered = self.filter(PublicationYearFilter(start_year, end_year))
+        filtered = sorted(filtered, key=lambda b: b.title.lower())
+        filtered = sorted(filtered, key=lambda b: int(b.publication_year))
+        return filtered
 
-def main():
-    BooksDataSource('/Users/zackdong/Documents/CS257/cs257-zimri/books/smalldatasource.csv')
-    # parse the args
-    # start with the full list of books
-    # for each filter (title, author, date)
-    #   remove books from the full list which don't match the filter
-    # sort by the specified sorting method (in the args)
-    # for each book remaining 
+    def filter(self, book_filter: BookFilter) -> List[Book]:
+        return list(filter(book_filter.check, self.all_books))
 
-if __name__ == "__main__":
-    main()
+
